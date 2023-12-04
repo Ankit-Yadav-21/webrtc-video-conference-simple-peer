1e049d109e119a99b1d9df177540d5d2766fdda6
Ankit-Yadav-21 <ciiankityadav21@gmail.com>
2023-12-04 17:56:06 +0530

    update design and update constraints

--- public/js/main.js
+++ public/js/main.js
@@ -11,13 +11,6 @@ let localStream = null;
  */
 let peers = {}
 
-// redirect if not https
-if (location.href.substr(0, 5) !== 'https')
-    location.href = 'https' + location.href.substr(4, location.href.length - 4)
-
-
-//////////// CONFIGURATION //////////////////
-
 /**
  * RTCPeerConnection configuration 
  */
@@ -56,14 +49,7 @@ const configuration = {
  */
 let constraints = {
     audio: true,
-    video: {
-        width: {
-            max: 300
-        },
-        height: {
-            max: 300
-        }
-    }
+    video: true
 }
 
 /////////////////////////////////////////////////////////
